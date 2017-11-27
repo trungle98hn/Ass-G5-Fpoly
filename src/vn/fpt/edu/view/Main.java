a0b8e9218904a4ec1f4e0d0ff1edcc73ae783200
Admin <Admin@trunglqph04966>
2017-11-27 14:11:28 +0700

    k

---
+++ src/vn/fpt/edu/view/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vn.fpt.edu.view;
+
+import javax.swing.JFrame;
+import javax.swing.*;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+    public static void main(String[] args) {
+          
+        FormQuanLy m = new FormQuanLy();
+      
+        m.setTitle("Quản lý siêu thị mini");
+        m.setSize(1000, 700);
+        m.setResizable(false);
+        m.setLocationRelativeTo(null);
+        m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        m.setVisible(true);
+    }
+   
+}
