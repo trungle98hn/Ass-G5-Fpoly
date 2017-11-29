1f2faf0e0714548fe63f44a1241804011fcc488b
Admin <Admin@trunglqph04966>
2017-11-29 23:29:33 +0700

    k

---
+++ src/vn/fpt/edu/beans/dangnhap.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vn.fpt.edu.beans;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Admin
+ */
+public class dangnhap implements Serializable{
+    String user,pass;
+
+    public dangnhap(String user, String pass) {
+        this.user = user;
+        this.pass = pass;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+}
